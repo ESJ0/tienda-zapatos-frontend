93dee186f250cb2df29309516fb7c657a4d7eb42
ESJ0 <usuarioj0s3p1@gmail.com>
2026-05-17 17:11:53 -0600

    Se configuro el deploy para github pages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    base: process.env.VITE_BASE_URL || '/',
     server: {
         port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true,
-            },
-        },
     },
 })
