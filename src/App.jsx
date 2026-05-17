25c6d05ad4e41b644cced804f02af294607d080e
ESJ0 <usuarioj0s3p1@gmail.com>
2026-05-17 17:44:41 -0600

    Se arreglo el routing con HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { CartProvider } from './context/CartContext'
 import ProtectedRoute from './routes/ProtectedRoute'
